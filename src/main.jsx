00a03f1fa30c0139d2cd0d616effe6b8d55709de
espadv69 <aitorjob69@gmail.com>
2025-01-15 13:19:07 +0100

    Add global css import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './components/css/global.css'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
