86926b12baedf1a4692ed90afb09a9a2e3074623
espadv69 <aitorjob69@gmail.com>
2025-01-15 18:21:33 +0100

    style: add button container to Menu component for improved layout

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,8 +4,18 @@ import './css/Menu.css'
 
 export const Menu = () => {
   return (
-    <main className='menu'>
+    <main className="menu">
       <h1 className="h1-menu-title">Character Creator</h1>
+
+      <div className="container-buttons-menu">
+        <div className="button-menu">
+          <button>Create</button>
+        </div>
+
+        <div className="button-menu">
+          <button>Show</button>
+        </div>
+      </div>
     </main>
   )
 }
