8f01b5571be788b66fcf13de5f40ad0c313ef901
espadv69 <aitorjob69@gmail.com>
2025-01-15 12:37:15 +0100

    Add React root element for rendering components

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <h1 className="h1-menu-title">Character Creator</h1>
+  </React.StrictMode>
+)
