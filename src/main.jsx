2e87d54a66af992f243679c5e0380a5931c05eae
espadv69 <aitorjob69@gmail.com>
2025-01-15 13:37:16 +0100

    Add Menu component

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import './components/css/global.css'
 
+import { Menu } from './components/Menu.jsx'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
-    <h1 className="h1-menu-title">Character Creator</h1>
+    <Menu />
   </React.StrictMode>
 )
