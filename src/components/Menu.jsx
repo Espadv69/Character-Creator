d19ff6e0f0dde90ef84e8e3f01faaa3d12265090
espadv69 <aitorjob69@gmail.com>
2025-01-16 12:22:02 +0100

    style: refactor Menu component to manage active state and improve structure

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,27 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import './css/Menu.css'
 
 export const Menu = () => {
+  const [activeComponent, setActiveComponent] = useState('menu')
+
   return (
     <main className="menu">
-      <h1 className="h1-menu-title">Character Creator</h1>
+      {activeComponent === 'menu' && (
+        <>
+          <h1 className="h1-menu-title">Character Creator</h1>
 
-      <div className="container-buttons-menu">
-        <div className="container-button-menu">
-          <button className="button-menu">Create</button>
-        </div>
+          <div className="container-buttons-menu">
+            <div className="container-button-menu">
+              <button className="button-menu">Create</button>
+            </div>
 
-        <div className="container-button-menu">
-          <button className="button-menu">Show</button>
-        </div>
-      </div>
+            <div className="container-button-menu">
+              <button className="button-menu">Show</button>
+            </div>
+          </div>
+        </>
+      )}
     </main>
   )
 }
