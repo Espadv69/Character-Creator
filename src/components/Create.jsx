2a7d5d3af9cdc1c3908d3f1542ea12ac8627581e
espadv69 <aitorjob69@gmail.com>
2025-01-16 20:28:46 +0100

    feat: add Elf option and image to Create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 import './css/Create.css'
 import MannequinImage from '../assets/mannequin.png'
+import ElfImage from '../assets/elf.png'
 
 export const Create = ({ setActiveComponent }) => {
   const [name, setName] = useState('')
@@ -9,18 +10,20 @@ export const Create = ({ setActiveComponent }) => {
 
   const setImage = () => {
     if (classType === 'mannequin') return MannequinImage
-    if (classType === 'goblin') return GoblinImage
+    if (classType === 'elf') return ElfImage
     return null
   }
 
   return (
     <div className="create">
+      <button onClick={() => setActiveComponent('menu')}>Go back</button>
       <div>
         <img className="img-create" src={setImage()} alt={classType} />
       </div>
 
       <select value={classType} onChange={(e) => setClassType(e.target.value)}>
         <option value="mannequin">Mannequin</option>
+        <option value="elf">Elf</option>
       </select>
     </div>
   )
