a6f54c5c644e82811d7e13848c141d20380f1b69
espadv69 <aitorjob69@gmail.com>
2025-01-17 12:42:19 +0100

    feat: add Dwarf option and image to Create component

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 
 import './css/Create.css'
+
 import MannequinImage from '../assets/mannequin.png'
 import ElfImage from '../assets/elf.png'
+import DwarfImage from '../assets/dwarf.png'
 
 export const Create = ({ setActiveComponent }) => {
   const [name, setName] = useState('')
@@ -11,6 +13,7 @@ export const Create = ({ setActiveComponent }) => {
   const setImage = () => {
     if (classType === 'mannequin') return MannequinImage
     if (classType === 'elf') return ElfImage
+    if (classType === 'dwarf') return DwarfImage
     return null
   }
 
@@ -24,6 +27,7 @@ export const Create = ({ setActiveComponent }) => {
       <select value={classType} onChange={(e) => setClassType(e.target.value)}>
         <option value="mannequin">Mannequin</option>
         <option value="elf">Elf</option>
+        <option value="dwarf">Dwarf</option>
       </select>
     </div>
   )
