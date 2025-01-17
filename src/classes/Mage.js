4310cf11a2bcbdbc907507683d11418f9b31d94a
espadv69 <aitorjob69@gmail.com>
2025-01-17 22:49:28 +0100

    fix: correct weapon name casing in Mage class constructor

--- src/classes/Mage.js
+++ src/classes/Mage.js
@@ -2,6 +2,6 @@ import Character from './Character'
 
 export default class Mage extends Character {
   constructor(name) {
-    super(name, 50, 20, 40, 100, 'staff')
+    super(name, 50, 20, 40, 100, 'Staff')
   }
 }
