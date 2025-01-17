ea5197831552ebb45bb12aff8866f3ff0cd2e5ae
espadv69 <aitorjob69@gmail.com>
2025-01-17 14:31:28 +0100

    feat: add image mapper utility for character images

---
+++ src/utils/imageMapper.js
@@ -0,0 +1,11 @@
+import MannequinImage from '../assets/mannequin.png'
+import ElfImage from '../assets/elf.png'
+import DwarfImage from '../assets/dwarf.png'
+import GoblinImage from '../assets/goblin.png'
+
+export const imagesMapper = {
+  mannequin: MannequinImage,
+  elf: ElfImage,
+  dwarf: DwarfImage,
+  goblin: GoblinImage,
+}
