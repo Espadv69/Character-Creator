dcf84eda37d761a8b7ef7009015029c742e4369c
espadv69 <aitorjob69@gmail.com>
2025-01-17 22:08:22 +0100

    feat: add Mage image to imageMapper for character representation

--- src/utils/imageMapper.js
+++ src/utils/imageMapper.js
@@ -2,10 +2,12 @@ import MannequinImage from '../assets/mannequin.png'
 import ElfImage from '../assets/elf.png'
 import DwarfImage from '../assets/dwarf.png'
 import GoblinImage from '../assets/goblin.png'
+import MageImage from '../assets/mage.png'
 
 export const imagesMapper = {
   mannequin: MannequinImage,
   elf: ElfImage,
   dwarf: DwarfImage,
   goblin: GoblinImage,
+  mage: MageImage,
 }
