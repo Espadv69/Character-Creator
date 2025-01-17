4cca22e8c0b0eb246d0b37f75016c5da1a2c67e0
espadv69 <aitorjob69@gmail.com>
2025-01-17 22:49:24 +0100

    fix: correct weapon name casing in Goblin class constructor

--- src/classes/Goblin.js
+++ src/classes/Goblin.js
@@ -2,6 +2,6 @@ import Character from './Character'
 
 export default class Goblin extends Character {
   constructor(name) {
-    super(name, 70, 35, 80, 10, 'dagger')
+    super(name, 70, 35, 80, 10, 'Dagger')
   }
 }
